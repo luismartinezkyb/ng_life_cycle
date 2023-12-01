7bdedcd5f8b20b6d7335f9f750e8bc00671165af
luismartinezkyb <18030545@itcelaya.edu.mx>
2023-12-01 08:14:00 -0600

    (feat): Implementación de standAlone side menu

--- src/app/alone/components/side-menu/side-menu.component.ts
+++ src/app/alone/components/side-menu/side-menu.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 interface MenuItem{
   name:string;
@@ -6,10 +8,20 @@ interface MenuItem{
 }
 
 @Component({
+  standalone:true,
   selector: 'maps-side-menu',
+  imports:[CommonModule, RouterModule],
   templateUrl: './side-menu.component.html',
   styles: [
     'li{ cursor: pointer; transition: 0.3s all}',
+    `
+    ul{
+      position:fixed;
+      top: 20px;
+      left: 20px;
+      z-index: 999;
+    }
+    `
   ] 
 })
 export class SideMenuComponent {
--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -8,19 +8,18 @@ import { environment } from 'src/environments/environments';
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MerkersPageComponent } from './pages/merkers-page/merkers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
     MiniMapComponent,
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MerkersPageComponent,
@@ -30,7 +29,8 @@ import { CounterAloneComponent } from '../alone/components/counter-alone/counter
   imports: [
     CommonModule,
     MapsRoutingModule,
-    CounterAloneComponent
+    CounterAloneComponent,
+    SideMenuComponent
   ]
 })
 export class MapsModule { }
